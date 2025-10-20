893cb197716d6007e7a2d5bfb32efd9772847071
Andi-Chandra <tugas.andi.chandra@gmail.com>
2025-10-20 15:17:51 +0700

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,15 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Z.ai Code Scaffold - AI-Powered Development",
+  // title: "Z.ai Code Scaffold - AI-Powered Development"
+  title: "PPS Belawan",
   description: "Modern Next.js scaffold optimized for AI-powered development with Z.ai. Built with TypeScript, Tailwind CSS, and shadcn/ui.",
   keywords: ["Z.ai", "Next.js", "TypeScript", "Tailwind CSS", "shadcn/ui", "AI development", "React"],
-  authors: [{ name: "Z.ai Team" }],
+  authors: [{ name: "Andi Candra" }],
   openGraph: {
     title: "Z.ai Code Scaffold",
     description: "AI-powered development with modern React stack",
-    url: "https://chat.z.ai",
+    url: "informasippsbelawan.vercel.app",
     siteName: "Z.ai",
     type: "website",
   },
